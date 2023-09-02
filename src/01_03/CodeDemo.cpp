fdb488c6891284a08c9106279081594b24ed5cc3
datttrian <datttrian@gmail.com>
2023-09-02 13:48:05 +0000

    Creating a simple program in C++

---
+++ src/01_03/CodeDemo.cpp
@@ -0,0 +1,13 @@
+// Learning C++
+// Exercise 01_03
+// Hello World, by Eduardo Corpeño
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	/* code */
+	cout << "Hi There!" << endl;
+	return 0;
+}
